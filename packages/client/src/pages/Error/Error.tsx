25b57351cc5f1c9aa7a1b4140a5608c751f6545d
Алексей Петренко <aipetrenko@corp.finam.ru>
2023-08-22 23:53:40 +0600

    feat: ErrorPage - 404, 500

--- packages/client/src/pages/Error/Error.tsx
+++ packages/client/src/pages/Error/Error.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react'
+import { Link } from 'react-router-dom'
+import styles from './Error.module.scss'
 
 interface IError {
   name: string
@@ -7,10 +9,12 @@ interface IError {
 
 const Error = (error: IError) => {
   return (
-    <div>
-      <div>Error</div>
-      <div>{error.name}</div>
-      <div>{error.text}</div>
+    <div className={styles.pageContainer}>
+      <h1 className={styles.errorName}>
+        {error.name}
+        <span className={styles.errorText}>{error.text}</span>
+      </h1>
+      <Link to="/">Вернуться на главную</Link>
     </div>
   )
 }
