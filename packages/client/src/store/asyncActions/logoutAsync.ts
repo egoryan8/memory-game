176de46bc5814e2ab42d06bc9d1c5155512613de
Алексей Петренко <aipetrenko@corp.finam.ru>
2023-08-30 23:09:20 +0600

    refactor: removed chains of promises and IIFE

--- packages/client/src/store/asyncActions/logoutAsync.ts
+++ packages/client/src/store/asyncActions/logoutAsync.ts
@@ -1,32 +1,28 @@
 import useStore from '@/store'
 import AuthApi from '@/api/AuthApi'
 
-const logoutAsync = () => ({
-  logoutAsync: () => {
-    const state = useStore.getState()
-    const { logout } = state
+const logoutAsync = () => {
+  return {
+    logoutAsync: async () => {
+      const state = useStore.getState()
+      const { logout } = state
 
-    ;(async () => {
-      AuthApi.logout()
-        .then(async response => {
-          return {
-            status: response.status,
-            text: await response.text(),
-          }
-        })
-        .then(response => {
-          switch (response.status) {
-            case 200:
-              logout()
-              break
-            default: {
-              console.log('LOGOUT_FAILED', response)
-            }
-          }
-        })
-        .catch(e => console.log('LOGOUT_FAILED', e.message))
-    })()
-  },
-})
+      try {
+        const response = await AuthApi.logout()
+        const text = await response.text()
+
+        switch (response.status) {
+          case 200:
+            logout()
+            break
+          default:
+            console.log('LOGOUT_FAILED', response)
+        }
+      } catch (e) {
+        console.log('LOGOUT_FAILED', (e as Error).message)
+      }
+    },
+  }
+}
 
 export default logoutAsync
