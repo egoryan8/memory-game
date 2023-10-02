05926ee32c351117db25c6e516c473e5df1d2417
Fogel Egor <fogel_92@bk.ru>
2023-10-03 00:48:23 +0700

    fix: tests

--- packages/client/src/App.test.tsx
+++ packages/client/src/App.test.tsx
@@ -7,6 +7,7 @@ import { setUserData } from '@/store/features/userSlice'
 import { mockUser } from '@/test/__mocks__/mockUser'
 import { act } from 'react-dom/test-utils'
 import { mockFetch } from '@/test/__mocks__/mockFetch'
+import { BrowserRouter } from 'react-router-dom'
 
 describe('App test', () => {
   beforeEach(() => mockFetch())
@@ -14,9 +15,11 @@ describe('App test', () => {
 
   it('Initial test', async () => {
     render(
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <BrowserRouter>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </BrowserRouter>
     )
     expect(screen.findByText('ВОЙТИ')).toBeDefined()
   })
@@ -24,9 +27,11 @@ describe('App test', () => {
   it('Check auth action', async () => {
     act(() => {
       render(
-        <Provider store={store}>
-          <App />
-        </Provider>
+        <BrowserRouter>
+          <Provider store={store}>
+            <App />
+          </Provider>
+        </BrowserRouter>
       )
       store.dispatch(setUserData(mockUser as IUser))
     })
