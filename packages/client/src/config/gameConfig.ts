6b6c3a4b681ea2719165eb01fa3797fbfa4f00f9
Алексей Петренко <aipetrenko@corp.finam.ru>
2023-10-07 13:26:55 +0600

    fix: reference error in gameConfig

--- packages/client/src/config/gameConfig.ts
+++ packages/client/src/config/gameConfig.ts
@@ -64,7 +64,7 @@ export const allIcons = [
   yarn,
 ]
 
-const screenHeight = window.innerHeight
+const screenHeight = typeof window !== 'undefined' ? window.innerHeight : 0
 
 const cardSize = {
   small: 85,
