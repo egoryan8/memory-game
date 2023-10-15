2d50fb7caea810f3bbe9420cafdc5d317f2db6c7
Aleksandr Peretertov <tailler2071@yandex.ru>
2023-10-15 17:44:50 +0500

    refactor: corrected remarks

--- packages/server/services/theme/theme.ts
+++ packages/server/services/theme/theme.ts
@@ -1,27 +1,8 @@
 import type { Handler } from 'express'
 import { Theme } from '../../models/theme/theme'
 
-export const allTheme: Handler = async (req, res) => {
-  if (req.headers.cookie) {
-    try {
-      const themes = await Theme.findAll()
-
-      if (themes) {
-        res.status(200).json({ themes })
-      } else {
-        res.status(404).json({ reason: 'Темы отсутствуют!' })
-      }
-    } catch (error) {
-      console.log(error)
-      res.status(500).json({ error: 'Internal Server Error' })
-    }
-  } else {
-    res.status(403).json({ message: 'Пользователь не авторизован!' })
-  }
-}
-
 export const setUserTheme: Handler = async (req, res) => {
-  if (req.headers.cookie && req.body) {
+  if (req.body) {
     const { theme, user_id } = req.body
 
     const newTheme = await Theme.create({
@@ -36,16 +17,20 @@ export const setUserTheme: Handler = async (req, res) => {
 }
 
 export const updateTheme: Handler = async (req, res) => {
-  const { user_id, theme } = req.body
+  if (req.body) {
+    const { user_id, theme } = req.body
 
-  try {
-    const [themeInstance, created] = await Theme.upsert(
-      { user_id, theme },
-      { returning: true }
-    )
-    res.json({ themeInstance, created })
-  } catch (error) {
-    res.status(500).send({ error: 'Ошибка при обновлении темы' })
+    try {
+      const [themeInstance, created] = await Theme.upsert(
+        { user_id, theme },
+        { returning: true }
+      )
+      res.status(200).json({ themeInstance, created })
+    } catch (error) {
+      res.status(500).send({ error: 'Ошибка при обновлении темы' })
+    }
+  } else {
+    res.status(404).json({ error: 'Данные не пришли или неполные' })
   }
 }
 
@@ -56,7 +41,7 @@ export const getTheme: Handler = async (req, res) => {
     const theme = await Theme.findOne({ where: { user_id } })
 
     if (theme) {
-      res.json(theme.theme)
+      res.status(200).json(theme.theme)
     } else {
       res.status(404).send({ error: 'Пользователь не найден' })
     }
