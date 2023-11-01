a78d081957587fd7c1b69954728068050f73a96a
volkovigor <info@viil.ru>
2023-11-02 02:03:18 +0500

    Add cache to ForumThreadUserAvatar.tsx

--- packages/client/src/pages/ForumThread/ForumThreadUserAvatar.tsx
+++ packages/client/src/pages/ForumThread/ForumThreadUserAvatar.tsx
@@ -3,19 +3,32 @@ import React, { useEffect, useState } from 'react'
 import DefaultAvatar from '@/assets/images/other/default-avatar-icon.svg'
 import s from './ForumThread.module.scss'
 
+// Создаем объект для кэширования аватаров
+const avatarCache = new Map<number, string>()
+
 const ForumThreadUserAvatar: React.FC<{ userId?: number; width?: string }> = ({
   userId,
   width = '20',
 }) => {
-  const [avatar, setAvatar] = useState<string>('')
+  const [avatar, setAvatar] = useState<string | undefined>('')
 
   const getUserById = async (id: number) => {
-    try {
-      const responseUser = await fetch(`${BASE_URI}/user/${id}`)
-      const userJson = await responseUser.json()
-      setAvatar(userJson.avatar)
-    } catch (error) {
-      console.error('Ошибка при получении данных:', error)
+    // Проверяем, есть ли аватар в кэше
+    if (avatarCache.has(id)) {
+      setAvatar(avatarCache.get(id))
+    } else {
+      try {
+        const responseUser = await fetch(`${BASE_URI}/user/${id}`)
+        const userJson = await responseUser.json()
+        const newAvatar = userJson.avatar
+
+        // Сохраняем аватар в кэше
+        avatarCache.set(id, newAvatar)
+
+        setAvatar(newAvatar)
+      } catch (error) {
+        console.error('Ошибка при получении данных:', error)
+      }
     }
   }
 
